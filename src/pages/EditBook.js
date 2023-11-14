3d8726480d49e3d17ff378341c3809e983b8e39d
Murad <smrtlearner202@gmail.com>
2023-11-14 16:46:55 +0530

    changes axios.put

--- src/pages/EditBook.js
+++ src/pages/EditBook.js
@@ -41,7 +41,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5000/books/${id}`, data)
+      .put(`https://mern-crud-six.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
